20237873927f970dbff1f43c1de011077c348660
Louis99Choi <73094206+Louis99Choi@users.noreply.github.com>
2021-02-19 12:55:43 +0900

    Just update

--- Lab2/src/Lab2_1/AreaCalculator.java
+++ Lab2/src/Lab2_1/AreaCalculator.java
@@ -2,21 +2,21 @@ package Lab2_1;
 
 public class AreaCalculator {
 
-	public static final int CIRCLE = 0; // circle
-	public static final int RECTANGLE = 1; // rectangle
-	public static final int TRIANGLE = 2; // triangle
-	public static final int TRAPEZOID = 3; // trapezoid
+	static final int CIRCLE = 0; // circle
+	static final int RECTANGLE = 1; // rectangle
+	static final int TRIANGLE = 2; // triangle
+	static final int TRAPEZOID = 3; // trapezoid
 	
-	public static double circleArea(double radius) {
+	static double circleArea(double radius) {
 		return Math.PI * radius * radius;
 	}
-	public static double rectangleArea(double length, double width) {
+	static double rectangleArea(double length, double width) {
 		return (length * width);
 	}
-	public static double triangleArea(double bottom, double height) {
+	static double triangleArea(double bottom, double height) {
 		return (bottom * height) / 2.0;
 	}
-	public static double trapezoidArea(double top, double bottom, double height) {
+	static double trapezoidArea(double top, double bottom, double height) {
 		return ((top + bottom) * height) / 2.0;
 	}
 }
